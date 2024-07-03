b643eec47eafebc09d3c68dfecd43c1286752f99
GamerSarath <sarath886@gmail.com>
2024-07-03 13:00:39 +0530

    second

---
+++ Assets/Real Game/Scripts/Mulatiplayer/PlayerControllerManager.cs
@@ -0,0 +1,150 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System.IO;
+using Photon.Realtime;
+using UnityEngine.UI;
+using System.Linq;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
+using Unity.VisualScripting;
+
+
+
+public class PlayerControllerManager : MonoBehaviourPunCallbacks
+{
+    PhotonView view;
+    GameObject controller;
+    GameObject[] enemyController;
+    public int playerTeam;
+    public int enemyTeam;
+    Dictionary<int, int> playerTeams = new Dictionary<int, int>();
+    bool  isStartingGame = false;
+    private void Awake()
+    {
+        view = GetComponent<PhotonView>();
+    }
+    private void Start()
+    {
+        if(view.IsMine)
+        {
+            CreateController();
+         
+        }
+    }
+
+
+    void CreateController()
+    {
+      if(PhotonNetwork.LocalPlayer.CustomProperties.ContainsKey("Team"))
+        {
+            playerTeam = (int)PhotonNetwork.LocalPlayer.CustomProperties["Team"];
+            Debug.Log("Players team is " + playerTeam);
+        }
+
+        AssignPlayerToSpawnArea(playerTeam);
+    }
+
+    public void Die()
+    {
+       controller.gameObject.SetActive(false);
+        CreateController(); 
+
+    }
+
+    void AssignPlayerToSpawnArea(int team)
+    {
+        GameObject spawnArea1 = GameObject.Find("SpawnArea1");
+        GameObject spawnArea2 = GameObject.Find("SpawnArea2");
+        GameObject[] botSpawnArea = new GameObject[EnemyNavigationScript.instance.spawnAreas.Length];
+
+        for (int i = 0; i < botSpawnArea.Length;i++)
+        {
+            botSpawnArea[i]  = EnemyNavigationScript.instance.spawnAreas[Random.Range(0, botSpawnArea.Length)];
+            enemyTeam = i % 2;
+            if(enemyTeam == 0)
+            {
+               //PhotonNetwork.LocalPlayer.CustomProperties["Team"] = enemyTeam;
+                playerTeams[i] = 1;
+            }
+            else
+            {
+                playerTeams[i] = 0;
+            }
+        }
+        if(spawnArea1 == null || spawnArea2 == null)
+        {
+            Debug.LogError("No Spawn Areas Specified");
+            return;
+        }
+
+        Transform spawnpoint = null;
+
+        if(team == 1)
+        {
+            spawnpoint = spawnArea1.transform.GetChild(Random.Range(0, spawnArea1.transform.childCount));
+        }
+        else
+        {
+            spawnpoint = spawnArea2.transform.GetChild(Random.Range(0, spawnArea2.transform.childCount));
+        }
+
+        if(spawnpoint != null)
+        {
+            enemyController = new GameObject[10];
+            if(!isStartingGame)
+            {
+                controller = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "Player"), spawnpoint.position, spawnpoint.rotation, 0, new object[] { view.ViewID });
+                Debug.Log("Instantiated Player Controller at spawn point ");
+
+                for (int i = 0; i < EnemyNavigationScript.instance.spawnAreas.Length; i++)
+                {
+                    Debug.Log("Instantiating Enemies" + i + botSpawnArea.Length);
+                    enemyController[i] = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "EnemyBot"), botSpawnArea[i].transform.position, botSpawnArea[i].transform.rotation, 0, new object[] { view.ViewID });
+                    enemyController[i].GetComponent<EnemyShooter>().enemyController = enemyController[i];
+                    if(spawnpoint.gameObject.tag == "Spawn1")
+                    {
+                        EnemyNavigationScript.instance.teamNumber = 2;
+                    }
+                    else
+                    {
+                        EnemyNavigationScript.instance.teamNumber = 1;
+                    }
+                }
+                isStartingGame = true;
+            }
+            else
+            {
+                controller.transform.position = spawnpoint.transform.position;
+                controller.gameObject.SetActive(true);
+
+                controller.GetComponent<PlayerMovement_RealGame>().Awake();
+            }
+            
+        }
+        else
+        {
+            Debug.LogError("No Available Spawn Point for team " + team);
+
+        }
+    }
+
+    void AssignTeamsToPlayers()
+    {
+        foreach(Photon.Realtime.Player player in PhotonNetwork.PlayerList)
+        {
+            if(player.CustomProperties.ContainsKey("Team"))
+            {
+                int _team = (int)player.CustomProperties["Team"];
+                playerTeams[player.ActorNumber] = _team;
+
+                AssignPlayerToSpawnArea(_team);
+            }
+        }
+    }
+
+    public override void OnPlayerEnteredRoom( Photon.Realtime.Player newPlayer)
+    {
+        AssignTeamsToPlayers();
+    }
+}
---
+++ Assets/Real Game/Scripts/PlayerMovement/PlayerMovement_RealGame.cs
@@ -0,0 +1,245 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+public class PlayerMovement_RealGame : MonoBehaviour
+{
+    InputManager inputManager;
+    PlayerManager playerManager;
+    Vector3 moveDirection;
+    AnimatorManager animatorManager;
+    Transform mainCam;
+    Animator animator;
+    Rigidbody playerRB;
+    [SerializeField] PlayerControllerManager playerControllerManager;
+    PhotonView view;
+    public Slider healthbarSlider;
+    public GameObject playerUI;
+
+    const float maxHealth = 200f;
+    public float currentHealth;
+    public float raycastradius;
+    public float movementSpeed = 2;
+    public float rotationSPeed = 13;
+    public float sprintSpeed = 5;
+    public bool isSprinting;
+    public bool isMoving;
+    public float inAirTimer;
+    public float leapingVelocity;
+    public float fallingVelocity;
+    public float raycastheightOffscet = 0.5f;
+    public LayerMask groundLayer;
+    public bool isGrounded ;
+    public float gravityIntensity = -50;
+    public float jumpHeight;
+    public bool isJumping;
+    public int playerTeam;
+    public void Awake()
+    {
+        currentHealth = maxHealth;
+        inputManager = GetComponent<InputManager>();
+        playerRB = GetComponent<Rigidbody>();
+        mainCam = Camera.main.transform;
+        playerManager = GetComponent<PlayerManager>();  
+        animatorManager = GetComponent<AnimatorManager>();
+        animator = GetComponent<Animator>();
+        view = GetComponent<PhotonView>();
+        playerControllerManager = PhotonView.Find((int)view.InstantiationData[0]).GetComponent<PlayerControllerManager>();
+        healthbarSlider.minValue = 0;
+        healthbarSlider.maxValue = maxHealth;
+        healthbarSlider.value = currentHealth;
+        
+    }
+    private void Start()
+    {
+        if(!view.IsMine)
+        {
+            Destroy(playerRB);
+            Destroy(playerUI);
+        }
+        if (view.Owner.CustomProperties.ContainsKey("Team"))
+        {
+            int team = (int)view.Owner.CustomProperties["Team"];
+            playerTeam = team;
+        }
+          
+    }
+    
+    public void HandleAllMovement()
+    {
+         HanldeFallingAndLanding();
+        if (playerManager.isInteracting)
+        {
+            return; 
+        }
+        HandleMovement();
+        HandleRotation();
+    }
+
+
+     void HandleMovement()
+    {
+        if (isJumping)
+            return;
+        
+        moveDirection = new Vector3(mainCam.forward.x, 0f, mainCam.forward.z) * inputManager.verticalInput;
+        moveDirection = moveDirection + mainCam.right * inputManager.horizontalInput;
+            
+        moveDirection.Normalize();
+
+        moveDirection.y = 0;
+
+       if(isSprinting ) 
+        {
+            moveDirection *= sprintSpeed;
+        }           
+        else
+        {
+            if(inputManager.movementAmount >= 0.5f)
+            {
+                moveDirection    *= movementSpeed;
+                isMoving = true;
+            }
+
+            if (inputManager.movementAmount <= 0f)
+            {
+                isMoving = false;
+            }
+
+        }
+
+        Vector3 movementVelocity = moveDirection;
+        playerRB.velocity = movementVelocity;
+
+
+    }
+
+     void HandleRotation()
+    {
+        if (isJumping)
+            return;
+        Vector3 targetDirection = Vector3.zero;
+
+        targetDirection = mainCam.forward * inputManager.verticalInput;
+        targetDirection = targetDirection +  mainCam.right * inputManager.horizontalInput;
+        targetDirection.Normalize();
+        targetDirection.y = 0;
+        raycastradius = 2;
+        if(targetDirection == Vector3.zero)
+        {
+            targetDirection = transform.forward;
+        }
+        Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+
+        Quaternion playerRotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSPeed * Time.deltaTime);
+        transform.rotation = playerRotation;
+
+    }
+
+    void HanldeFallingAndLanding()
+    {
+        RaycastHit hit;
+
+        Vector3 raycastOrigin = transform.position;
+        Vector3 targetPositiion;
+        raycastOrigin.y = raycastOrigin.y + raycastheightOffscet;
+        targetPositiion = transform.position;
+
+        if(!isGrounded && !isJumping)
+        {
+            if (!playerManager.isInteracting)
+            { 
+                animatorManager.PlayTargetAnimation("Falling", true);
+            }
+
+            inAirTimer += Time.deltaTime;
+            playerRB.AddForce(transform.forward *leapingVelocity);
+            playerRB.AddForce(-Vector3.up * fallingVelocity * inAirTimer);
+
+        }
+        bool r = Physics.SphereCast(raycastOrigin, .2f  , -Vector3.up, out hit, groundLayer);
+       // Debug.Log(r);
+        if (r)
+        {
+            if (!isGrounded && !playerManager.isInteracting)
+            {
+                animatorManager.PlayTargetAnimation("Landing", true);
+               // playerManager.isInteracting = false;
+                //  animator.SetBool("isInteracting", false);
+            }
+
+            Vector3 raycasthitPoint = hit.point;
+            targetPositiion.y = raycasthitPoint.y;
+            inAirTimer = 0;
+            isGrounded = true;
+        }
+        else 
+        {
+            isGrounded = false;
+           
+        }
+        if(isGrounded && !isJumping)
+        {
+
+            if(playerManager.isInteracting ||  inputManager.movementAmount > 0)
+            {
+                transform.position = Vector3.Lerp(transform.position, targetPositiion, Time.deltaTime / 0.1f);
+            }
+            else
+            {
+                transform.position = targetPositiion;
+            }
+        }
+    }
+
+    public void HandleJumping()
+    {
+        if(isGrounded)
+        {
+            animator.SetBool("isJumping", true);
+            animatorManager.PlayTargetAnimation("Jump", false);
+
+            float jumpingVelocity = Mathf.Sqrt(-2 * gravityIntensity * jumpHeight);
+            Vector3 playerVelocity = moveDirection;
+            playerVelocity.y = jumpingVelocity;
+            playerRB.velocity = playerVelocity;
+            isJumping = false;
+        }
+    }
+
+    public void SetJumping(bool jumping)
+    {
+        isJumping = jumping;
+    }
+    public void ApplyDamage(float damageValue)
+    {
+        view.RPC(nameof(RPC_TakeDamage), RpcTarget.All, damageValue);
+    }
+
+    [PunRPC]
+
+    void RPC_TakeDamage(float damage)
+    {
+        if(!view.IsMine)
+        {
+            return;
+        }
+        currentHealth -= damage;
+        healthbarSlider.value = currentHealth;      
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
+        Debug.Log("Damage taken" + damage);
+        Debug.Log("Current Health is" + currentHealth   );
+
+    }
+
+    void Die()
+    {
+        playerControllerManager.Die();
+        ScoreBoard.Instance.PlayerDied(playerTeam);
+    }
+}
+        
